eeb8eec18ea5712c9c9f7089e60045f8a767bfd3
Sh-Zh-7 <2431297348@qq.com>
2021-02-01 14:03:20 +0800

    feat: add scanner and command line interface.

---
+++ src/io/github/tl/tl.java
@@ -0,0 +1,49 @@
+package io.github.tl;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Scanner;
+import java.util.stream.Stream;
+
+public class tl {
+    public static void main(String[] args) throws IOException {
+        if (args.length > 1) {
+            System.out.println("Usage: tl [script]");
+            System.exit(64);
+        } else if (args.length == 1) {
+            runFile(args[0]);
+        } else {
+            runPrompt();
+        }
+    }
+
+    private static void runPrompt() throws IOException {
+        InputStreamReader inputStreamReader = new InputStreamReader(System.in);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+        while (true) {
+            System.out.print(">>> ");
+            String line = bufferedReader.readLine();
+            if (line == null) {
+                break;
+            }
+            run(line);
+        }
+    }
+
+    private static void runFile(String path) throws IOException {
+        byte[] bytes = Files.readAllBytes(Paths.get(path));
+        run(new String(bytes, Charset.defaultCharset()));
+    }
+
+    private static void run(String source) {
+        Scanner scanner = new Scanner(source);
+        Stream<String> tokens = scanner.tokens();
+
+        tokens.forEach(System.out::println);
+    }
+}
