0fabc453f8c4a0792f5cebf948e9a25a6a5bb1bc
Sh-Zh-7 <2431297348@qq.com>
2021-02-03 16:33:45 +0800

    feat: add interpreter file, now we have a calculator.

---
+++ src/io/github/tl/error/RuntimeError.java
@@ -0,0 +1,12 @@
+package io.github.tl.error;
+
+import io.github.tl.scan.Token;
+
+public class RuntimeError extends RuntimeException {
+    public final Token token;
+
+    public RuntimeError(Token token, String message) {
+        super(message);
+        this.token = token;
+    }
+}
