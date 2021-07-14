5399d8de5dc1a740295d2e4cb75bb2dacae2fea8
Sh-Zh-7 <zhihaoshen7@qq.com>
2021-07-14 11:02:51 +0800

    feat: support branch statement.

--- src/main/java/io/github/tl/interpret/Interpreter.java
+++ src/main/java/io/github/tl/interpret/Interpreter.java
@@ -169,7 +169,7 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 
     @Override
     public Void visitIfStmt(Stmt.If stmt) {
-        if (isTruthy(stmt.condition)) {
+        if (isTruthy(evaluate(stmt.condition))) {
             execute(stmt.thenBranch);
         } else if (stmt.elseBranch != null) {
             execute(stmt.elseBranch);
